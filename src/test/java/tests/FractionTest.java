2284600c81dbed534bc3cab97e872c9a0ef1929a
FlorSalcedoWD <florenciasalcedo13@gmail.com>
2020-02-15 23:47:13 -0300

    created classes FractionTest & UserTest - Issue #1

---
+++ src/test/java/tests/FractionTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FractionTest {
+
+}
