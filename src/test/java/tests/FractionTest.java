0815c51ec9a36f3502fa203543cb3fc91e9a31d0
FlorSalcedoWD <florenciasalcedo13@gmail.com>
2020-02-15 23:52:48 -0300

    tests created and passed - Issue #1

--- src/test/java/tests/FractionTest.java
+++ src/test/java/tests/FractionTest.java
@@ -6,5 +6,29 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FractionTest {
+    private Fraction fraction;
 
+    @BeforeEach
+    void before(){
+        this.fraction = new Fraction(4, 5);
+    }
+
+    @Test
+    public void testFraction(){
+        this.fraction = new Fraction();
+        assertEquals(1, this.fraction.decimal());
+    }
+
+    @Test
+    public void testFraction2(){
+        this.fraction = new Fraction(4, 5);
+        assertEquals(0.8, this.fraction.decimal(), 10e-5);
+    }
+
+    @Test
+    public void testDecimal(){
+        this.fraction = new Fraction(6, 5);
+        double res = this.fraction.decimal();
+        assertEquals(1.2, res, 10e-5);
+    }
 }
--- src/test/java/tests/UserTest.java
+++ src/test/java/tests/UserTest.java
@@ -6,5 +6,45 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserTest {
+    private User user;
 
+    @BeforeEach
+    void before(){
+        this.user = new User(5, "florencia", "salcedo");
+    }
+
+    @Test
+    void testUser(){
+        this.user = new User(6, "Lola", "Ponce");
+        assertEquals(6, this.user.getNumber());
+        assertEquals("Lola Ponce", this.user.fullName());
+    }
+
+    @Test
+    void testFullName(){
+        String nombre = this.user.fullName();
+        assertEquals("Florencia Salcedo", nombre);
+    }
+
+    @Test
+    void testGetNumber(){
+        int var = this.user.getNumber();
+        assertEquals(5, var);
+    }
+
+    @Test
+    void testInitials(){
+        String var = this.user.initials();
+        assertEquals("F.", var);
+    }
+
+    @Test
+    void testGetFamilyName(){
+        assertEquals("Salcedo", this.user.getFamilyName());
+    }
+
+    @Test
+    void stestGetName(){
+        assertEquals("Florencia", this.user.getName());
+    }
 }
